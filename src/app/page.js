a5608adc6806ae76469140df3e7dbace14823b96
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-08 18:10:17 +0200

    Ejercicio 1 completado

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,7 @@ const page = () => {
   return (
     <div>
       <ExerciseOne/>
-      <ExerciseTwo/>
-      <ExerciseThree/>
+    
     </div>
   )
 }
--- src/components/ExerciseOne.jsx
+++ src/components/ExerciseOne.jsx
@@ -1,29 +1,40 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const ExerciseOne = () => {
-  
+  const [showText, setShowText] = useState("");
   const inputNameRef = useRef(null);
   const inputLastNameRef = useRef(null);
   const inputAgeRef = useRef(null);
   const inputCityRef = useRef(null);
 
+  const printValue = (event) => {
+    event.preventDefault();
+    setShowText(
+      `${inputNameRef.current.value} ${inputLastNameRef.current.value} tiene ${inputAgeRef.current.value} años y vive en ${inputCityRef.current.value}`
+    );
+    event.target.reset()
+  };
+
   return (
-    <div>
-      <form>
+    <div className="flex flex-col justify-center items-center p-10">
+      <form onSubmit={printValue} className="flex flex-col w-[300px] h-[250px] bg-slate-400 ">
         <label htmlFor="firstName">Nombre:</label>
         <input type="text" name="firstName" id="firstName" ref={inputNameRef} />
         <label htmlFor="lastName">Apellido:</label>
-        <input type="text" name="lastName" id="lastName" ref={inputLastNameRef}/>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          ref={inputLastNameRef}
+        />
         <label htmlFor="age">Edad:</label>
-        <input type="number" name="age" id="age" ref={inputCityRef} />
+        <input type="number" name="age" id="age" ref={inputAgeRef} />
         <label htmlFor="city">Ciudad:</label>
-        <input type="text" name="city" id="city" ref={inputCityRef}/>
-        <input type="submit" />
+        <input type="text" name="city" id="city" ref={inputCityRef} />
+        <input type="submit" value={"Enviar"}/>
       </form>
-      <h1>
-        {inputNameRef.current.value} {inputLastNameRef.current.value} tiene {inputAgeRef.current.value} años y vive en {inputCityRef.current.value} 
-      </h1>
+      <h1>{showText}</h1>
     </div>
   );
 };
